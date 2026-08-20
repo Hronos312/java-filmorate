7eae36212546cb64cb8fe705f8f942101d0be5da
VlaDosick <Mlgtrack@yandex.ru>
2026-08-20 22:57:52 +0300

    fix checkStyle

--- src/main/java/ru/yandex/practicum/filmorate/model/Review.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -10,6 +11,7 @@ import java.time.LocalDate;
 @Data
 public class Review {
 
+    @JsonProperty("reviewId")
     private Long id;
 
     @NotNull(message = "ID фильма должен быть указан")
